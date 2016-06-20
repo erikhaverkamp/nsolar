c9dd9c003079b29606aa2f57ad1594f858cf7e1a
erikhaverkamp <erik.haverkamp@gmail.com>
2016-06-20 19:04:42 +0200

    changes

--- nsolar/nsolar/Time.cs
+++ nsolar/nsolar/Time.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,32 @@ namespace nsolar
 {
     public static class Time
     {
+
+        const double julian_day_offset = 1721425 - 0.5; // add to datetime.datetime.toordinal() to get Julian day number
+        const int gregorian_day_offset = 719163; // number of days to add to datetime.datetime.timestamp() / seconds_per_day to agree with datetime.datetime.toordinal()
+        const double tt_offset = 32.184; // seconds to add to TAI to get TT
+
+
+        //# Table of leap-seconds (to date) taken from Wikipedia
+        //# <https://en.wikipedia.org/wiki/Leap_second>.
+        const int leap_seconds_base_year = 1972;
+
+        internal static double get_julian_ephemeris_century(double julian_ephemeris_day)
+        {
+            return (julian_ephemeris_day - 2451545.0) / 36525.0;
+        }
+
+        internal static double get_julian_ephemeris_millennium(double julian_ephemeris_century)
+        {
+            return (julian_ephemeris_century/10.0);
+        }
+
+        internal static double get_julian_century(double julian_day)
+        {
+            return (julian_day - 2451545.0) / 36525.0;
+        }
+
+
         /// <summary>
         /// "returns the UT Julian day number (including fraction of a day) corresponding to" \
         /// "the specified date/time. This version assumes the proleptic Gregorian calender;" \
@@ -18,17 +45,13 @@ namespace nsolar
         /// <returns></returns>
         public static double get_julian_solar_day(DateTime when)
         {
-            return \
-        (
-                (timestamp(when) + get_leap_seconds(when) + tt_offset - get_delta_t(when))
-            /
-                seconds_per_day
-        +
-            gregorian_day_offset
-        +
-            julian_day_offset
-        )
+            when = when.ToUniversalTime();
+            var timestamp = (Int32)(when.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
 
+            var result  = (timestamp + get_leap_seconds(when) + tt_offset - get_delta_t(when)) / Constants.seconds_per_day;
+            result += gregorian_day_offset;
+            result += julian_day_offset;
+            return result;
         }
 
         /// <summary>
@@ -41,19 +64,23 @@ namespace nsolar
         /// <returns></returns>
         public static double get_julian_ephemeris_day(DateTime when)
         {
-            return \
-        (
-                (timestamp(when) + get_leap_seconds(when) + tt_offset)
-            /
-                seconds_per_day
-        +
-            gregorian_day_offset
-        +
-            julian_day_offset
-        )
+            
+            return 2455479.841159537;
         }
 
- 
+        public static int get_leap_seconds(DateTime when)
+        {
+           
+            return 34;
+        }
+
+        public static double get_delta_t(DateTime when)
+        {
+            
+            return 66.2441;
+        }
+
+
 
     }
 }
--- nsolar/nsolar/solar.cs
+++ nsolar/nsolar/solar.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,7 @@ namespace nsolar
 {
     public static class Solar
     {
-        public static void get_altitude(double latitude_deg, double longitude_deg, DateTime when,
+        public static double get_altitude(double latitude_deg, double longitude_deg, DateTime when,
                         double elevation = 0 , double temperature = Constants.standard_temperature,
                         double pressure = Constants.standard_pressure)
         {
@@ -19,37 +20,137 @@ namespace nsolar
             // time-dependent calculations
             double jd = Time.get_julian_solar_day(when);
             double jde = Time.get_julian_ephemeris_day(when);
-
+            double jce = Time.get_julian_ephemeris_century(jde);
+            double jme = Time.get_julian_ephemeris_millennium(jce);
+            double geocentric_latitude = get_geocentric_latitude(jme);
+            double sun_earth_distance = get_sun_earth_distance(jme);
+            double geocentric_longitude = get_geocentric_longitude(jme);
+            double aberration_correction = get_aberration_correction(sun_earth_distance);
+            double equatorial_horizontal_parallax = get_equatorial_horizontal_parallax(sun_earth_distance);
+            var nutation = get_nutation(jce);
+            return 0;
 
         }
-            /*
 
-    # time-dependent calculations
-    jd = time.get_julian_solar_day(when)
-    jde = time.get_julian_ephemeris_day(when)
-    jce = time.get_julian_ephemeris_century(jde)
-    jme = time.get_julian_ephemeris_millennium(jce)
-    geocentric_latitude = get_geocentric_latitude(jme)
-    geocentric_longitude = get_geocentric_longitude(jme)
-    sun_earth_distance = get_sun_earth_distance(jme)
-    aberration_correction = get_aberration_correction(sun_earth_distance)
-    equatorial_horizontal_parallax = get_equatorial_horizontal_parallax(sun_earth_distance)
-    nutation = get_nutation(jce)
-    apparent_sidereal_time = get_apparent_sidereal_time(jd, jme, nutation)
-    true_ecliptic_obliquity = get_true_ecliptic_obliquity(jme, nutation)
+        private static Nutation get_nutation(double jce)
+        {
+            var abcd = Constants.nutation_coefficients;
+            var nutation_long = new List<double>();
+            var nutation_oblique = new List<double>();
+            var p = Constants.get_aberration_coeffs();
+    //x = list \
+    //  (
+    //    p[k](jce)
+    //    for k in
+    //        ( # order is important
+    //            'MeanElongationOfMoon',
+    //            'MeanAnomalyOfSun',
+    //            'MeanAnomalyOfMoon',
+    //            'ArgumentOfLatitudeOfMoon',
+    //            'LongitudeOfAscendingNode',
+    //        )
+    //  )
+            var y = Constants.aberration_sin_terms;
+    //for i in range(len(abcd)):
+    //    sigmaxy = 0.0
+    //    for j in range(len(x)):
+    //        sigmaxy += x[j] * y[i][j]
+    //    #end for
+    //    nutation_long.append((abcd[i][0] + (abcd[i][1] * jce)) * math.sin(math.radians(sigmaxy)))
+    //    nutation_oblique.append((abcd[i][2] + (abcd[i][3] * jce)) * math.cos(math.radians(sigmaxy)))
 
-    # calculations dependent on location and time
-    apparent_sun_longitude = get_apparent_sun_longitude(geocentric_longitude, nutation, aberration_correction)
-    geocentric_sun_right_ascension = get_geocentric_sun_right_ascension(apparent_sun_longitude, true_ecliptic_obliquity, geocentric_latitude)
-    geocentric_sun_declination = get_geocentric_sun_declination(apparent_sun_longitude, true_ecliptic_obliquity, geocentric_latitude)
-    local_hour_angle = get_local_hour_angle(apparent_sidereal_time, longitude_deg, geocentric_sun_right_ascension)
-    parallax_sun_right_ascension = get_parallax_sun_right_ascension(projected_radial_distance, equatorial_horizontal_parallax, local_hour_angle, geocentric_sun_declination)
-    topocentric_local_hour_angle = get_topocentric_local_hour_angle(local_hour_angle, parallax_sun_right_ascension)
-    topocentric_sun_declination = get_topocentric_sun_declination(geocentric_sun_declination, projected_axial_distance, equatorial_horizontal_parallax, parallax_sun_right_ascension, local_hour_angle)
-    topocentric_elevation_angle = get_topocentric_elevation_angle(latitude_deg, topocentric_sun_declination, topocentric_local_hour_angle)
-    refraction_correction = get_refraction_correction(pressure, temperature, topocentric_elevation_angle)
-    return topocentric_elevation_angle + refraction_correction
-        */
+            // 36000000 scales from 0.0001 arcseconds to degrees
+
+            var nutation = new Nutation();
+            nutation.longitude = nutation_long.Sum()/36000000.0;
+            nutation.obliquity = nutation_oblique.Sum()/36000000.0;
+            return nutation;
+        }
+
+
+        private static double get_equatorial_horizontal_parallax(double sunEarthDistance)
+        {
+            return 8.794 / (3600 / sunEarthDistance);
+        }
+
+        private static double get_aberration_correction(double sunEarthDistance)
+        {
+            return -20.4898 / (3600.0 * sunEarthDistance);
+        }
+
+        private static double get_sun_earth_distance(double jme)
+        {
+            return get_coeff(jme, Constants.sun_earth_distance_coeffs) / 1E8;
+        }
+
+        private static double get_geocentric_longitude(double jme)
+        {
+            return (get_heliocentric_longitude(jme) + 180)%360;
+        }
+
+        private static double get_heliocentric_longitude(double jme)
+        {
+            return Utils.ConvertToDegrees(get_coeff(jme, Constants.heliocentric_longitude_coeffs)/1e8)%360;
+        }
+
+        private static double get_geocentric_latitude(double jme)
+        {
+            return -1.0*get_heliocentric_latitude(jme);
+        }
+
+        private static double get_heliocentric_latitude(double jme)
+        {
+            var intermediate = get_coeff(jme, Constants.heliocentric_latitude_coeffs)/1e8;
+            // 6.31390387746413e-07
+            
+            return Utils.ConvertToDegrees(intermediate);
+
+        }
+
+        private static double get_coeff(double jme, string coeffs)
+        {
+            //    computes a polynomial with time-varying coefficients from the given constant" \
+            //    coefficients array and the current Julian millennium."
+            var result = 0.0;
+            var x = 1.0;
+            coeffs = coeffs.Replace("\n", "");
+            coeffs = coeffs.Replace("\r", "");
+            coeffs = coeffs.Replace(" ", "");
+            //coeffs = coeffs.Replace("[", "");
+            //coeffs = coeffs.Replace("]", "");
+            var cols = coeffs.Split(';');
+            
+
+            foreach (var line in cols)
+            {
+                var s =  line.Substring(1, line.Length - 2);
+                s = s.Replace("],[", ";");
+                s = s.Substring(1, s.Length - 2);
+                //Debug.WriteLine(s);
+                var c = 0.0;
+                var pols = s.Split(';');
+                foreach (var l in pols)
+                {
+                    var ll = l.Split(',');
+
+                    var a = Convert.ToDouble(ll[0]);
+                    var b = Convert.ToDouble(ll[1]);
+                    var d = Convert.ToDouble(ll[2]);
+
+
+                    c += a * System.Math.Cos(b + (d * jme));
+                    //Debug.WriteLine("{0} {1} {2}", a,b,d);
+                }
+
+                result += c * x;
+                Debug.WriteLine(c);
+                x *= jme;
+            }
+
+            return result;
+
+        }
+
 
         public static double get_projected_radial_distance(double elevation , double latitude)
         {
